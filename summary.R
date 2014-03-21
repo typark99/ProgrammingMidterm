94eeeec4b707be47c4c18b7185fa2feddee52f11
typark99 <typark99@gmail.com>
2014-03-21 12:20:29 -0500

    Revised summary()

--- summary.R
+++ summary.R
@@ -2,7 +2,7 @@
 #'
 #' Returns the output of the posterior expected coefficients and posterior probability that the coefficient s non-zero from fitBMA()
 #'
-#' @param x A \code{RegStats} object
+#' @param x A \code{BMA} object
 #' 
 #' @return NULL
 #' @author Taeyong Park
@@ -16,18 +16,18 @@
 #' @aliases summary,BMA-method
 #' @rdname summary
 #' @export
-setMethod(f="print",  # Since print is a built-in function we do not set a generic function, and define the print function used under the Regressions class  
+setMethod(f="summary",  # Since print is a built-in function we do not set a generic function, and define the print function used under the Regressions class  
           signature="BMA",
-          definition=function(x){
+          definition=function(object){
             cat("*** Start Summary (Class-BMA) ***", "\n", "\n")
             cat("1) Posterior expected value of each coefficient", "\n")
-            for(i in 1:length(x@output$postCoef)){  
-              cat("Coefficient", i, ":", x@output$postCoef[i], "\n") # Of the output of fitBMA(), postCoef indicates the posterior expected value of each coefficient
+            for(i in 1:length(object@output$postCoef)){  
+              cat("Coefficient", i, ":", round(object@output$postCoef[i],4), "\n") # Of the output of fitBMA(), postCoef indicates the posterior expected value of each coefficient
             }
-            cat("2) The posterior probability that the coefficient is non-zero", "\n")
-            for(i in 1:length(x@output$probSig)){ # for loop enables to separate each model 
-              cat("Coefficient", i, ":", x@output$probSig[i], "\n") # Of the output of fitBMA(), probSig indicates The posterior probability that the coefficient is non-zero
+            cat("\n", "2) Posterior probability that the coefficient is non-zero", "\n", sep="")
+            for(i in 1:length(object@output$probSig)){ # for loop enables to separate each model 
+              cat("Coefficient", i, ":", round(object@output$probSig[i],4), "\n") # Of the output of fitBMA(), probSig indicates The posterior probability that the coefficient is non-zero
             }
-            cat("\n","**** End Summary (Class-BMA) ****")
+            cat("\n","**** End Summary (Class-BMA) ****", sep="")
           }
 )
