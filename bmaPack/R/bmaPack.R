ea871eb5ca2ffdd7d4aa29fa7ed20891c4ee45f8
typark99 <typark99@gmail.com>
2014-03-21 13:45:45 -0500

    Created developmentDocument file; Installed the package

---
+++ bmaPack/R/bmaPack.R
@@ -0,0 +1,7 @@
+#' bmaPack
+#'
+#' The bmaPack performs Bayesian modeling average (BMA) 
+#' @name bmaPack
+#' @docType package
+#' @author  Taeyong Park: \email{typark99@@gmail.com} 
+NULL
---
+++ developmentDocument.R
@@ -0,0 +1,23 @@
+## Load libraries and set working directory
+library(devtools)
+library(roxygen2)
+setwd("~/GitHub/ProgrammingMidterm") # Set my working directory
+
+## At this point put the *.R files into the correct directories and edit the DESCRIPTION file
+
+## The R directory is in this order:
+
+# bmaPack.r
+# BMA.R
+# fitBMA.R
+# plot.R
+# summary.R
+
+
+## This can be run many times as the code is updates
+current.code <- as.package("bmaPack")
+load_all(current.code) # Load all of the functions so you can use them
+document(current.code) # Make the help files
+check(current.code) # Run the R checks
+install(pkg=current.code, local=TRUE) # Install the package
+
